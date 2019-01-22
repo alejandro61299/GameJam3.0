0a485c4ae1443b0d028148470d7fb5aacb675cf6
AAurelioGN <monsterlol64@gmail.com>
2019-01-22 14:27:19 +0100

    Updated inventory manager

---
+++ SorpresaGameJam/Assets/Scripts/MainGame/InventoryManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour {
+
+    public GameObject[] propInventory;
+    public bool[] activedProps;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
